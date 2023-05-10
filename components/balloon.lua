418724487596f52884eb8f1321476d8db82ffe8d
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-05-10 18:30:12 -0300

    :bug: fix: Fixed text wrapping function

--- components/balloon.lua
+++ components/balloon.lua
@@ -41,15 +41,23 @@ end
 function balloon:setTiles_n()
   self.tiles_n.x= math.floor((_G.screen.w>self.max_width and self.max_width or _G.screen.w)/(self.tile.size.w*self.s.x))
   self.spacing.w= (_G.screen.w-(self.tiles_n.x*self.tile.size.w))/2
-  self.size= (self.tiles_n.x*self.tile.size.w)
+  self.width_msg= (self.tiles_n.x*self.tile.size.w)
 end
 
 function balloon:update()
   self:setTiles_n()
+  self:quebra_linhas_msg()
+end
+
+function balloon:calc_lines_n(text_w)
+  return math.ceil(text_w*2/(self.width_msg-(self.text_spacing*2)))
+end
+
+function balloon:quebra_linhas_msg()
   if #self.messages>0 then
     lines= {}
     local text_w= font:getWidth(self.messages[self.i])
-    lines_n= math.ceil(text_w*2/(self.size-(self.text_spacing*2)))
+    lines_n= self:calc_lines_n(text_w)
     local pi= 1
     local pf= 2
     local subtext= 'test'
@@ -58,13 +66,15 @@ function balloon:update()
       if #lines>0 then pi= utf8.offset(self.messages[self.i], pf) end
       pf= pi + 1
       subtext= self.messages[self.i]:sub(pi, utf8.offset(self.messages[self.i], pf)-1)
-      while font:getWidth(subtext)<(self.size/2)-(self.text_spacing*2) do
+      while font:getWidth(subtext)<(self.width_msg/2)-(self.text_spacing*2) do
         pf= pf + 1
         subtext= self.messages[self.i]:sub(pi, utf8.offset(self.messages[self.i], pf)-1)
       end
       lines[#lines+1]= subtext
     end
-    pi= utf8.offset(self.messages[self.i], pf)
+    if #lines>1 then pi= utf8.offset(self.messages[self.i], pf)
+    else pi= utf8.offset(self.messages[self.i], pi)
+    end
     subtext= self.messages[self.i]:sub(pi, utf8.offset(self.messages[self.i], utf8.len(self.messages[self.i])))
     lines[#lines+1]= subtext
   end
@@ -72,37 +82,45 @@ end
 
 function balloon:draw()
   if #self.messages>0 then
-    for i=1, self.tiles_n.x do
-      for j=1, 3 do
-        if (i==1 and j==1) then
-          love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, self.s.x, self.s.y)
-        elseif (i==1 and j==3) then
-          love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), self.angle, self.s.x, -math.abs(self.s.y))
-        elseif (i==self.tiles_n.x and j==1) then
-          love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), self.s.y)
-        elseif (i==self.tiles_n.x and j==3) then
-          love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), -math.abs(self.s.y))
-        elseif (j==1) then
-          love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), math.rad(90), self.s.x, self.s.y, 0, self.tile.size.h)
-        elseif (j==3) then
-          love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), math.rad(270), self.s.x, self.s.y)
-        elseif (i==1) then
-          love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, self.s.x, self.s.y)
-        elseif (i==self.tiles_n.x) then
-          love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), self.s.y, self.tile.size.w, 0)
-        elseif ((i>1 and i<self.tiles_n.x) and (j>1 and j<3)) then
-          love.graphics.rectangle("fill", self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.tile.size.w*self.s.x, self.tile.size.h*self.s.y)
-        end
-      end
-    end
-    love.graphics.setColor(0/255, 0/255, 0/255, 255/255)
-    if #lines==lines_n then
-      for i=1, lines_n do
-        love.graphics.print(lines[i], self.spacing.w+self.text_spacing, self.spacing.h+self.text_spacing+((i-1)*15*2), self.angle, 2, 2)
-      end
-    end
-    love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+    self:desenha_forma()
+    self:escreve_texto()
   end
+end
+
+function balloon:desenha_forma()
+  for i=1, self.tiles_n.x do
+    for j=1, 3 do
+      if (i==1 and j==1) then
+        love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, self.s.x, self.s.y)
+      elseif (i==1 and j==3) then
+        love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), self.angle, self.s.x, -math.abs(self.s.y))
+      elseif (i==self.tiles_n.x and j==1) then
+        love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), self.s.y)
+      elseif (i==self.tiles_n.x and j==3) then
+        love.graphics.draw(self.img.obj, self.objs.edge, self.spacing.w+((i)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), -math.abs(self.s.y))
+      elseif (j==1) then
+        love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), math.rad(90), self.s.x, self.s.y, 0, self.tile.size.h)
+      elseif (j==3) then
+        love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j)*self.tile.size.h*self.s.y), math.rad(270), self.s.x, self.s.y)
+      elseif (i==1) then
+        love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, self.s.x, self.s.y)
+      elseif (i==self.tiles_n.x) then
+        love.graphics.draw(self.img.obj, self.objs.corner, self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.angle, -math.abs(self.s.x), self.s.y, self.tile.size.w, 0)
+      elseif ((i>1 and i<self.tiles_n.x) and (j>1 and j<3)) then
+        love.graphics.rectangle("fill", self.spacing.w+((i-1)*self.tile.size.w*self.s.x), self.spacing.h+((j-1)*self.tile.size.h*self.s.y), self.tile.size.w*self.s.x, self.tile.size.h*self.s.y)
+      end
+    end
+  end
+end
+
+function balloon:escreve_texto()
+  love.graphics.setColor(0/255, 0/255, 0/255, 255/255)
+  if #lines==lines_n then
+    for i=1, lines_n do
+      love.graphics.print(lines[i], self.spacing.w+self.text_spacing, self.spacing.h+self.text_spacing+((i-1)*15*2), self.angle, 2, 2)
+    end
+  end
+  love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
 end
 
 return balloon
